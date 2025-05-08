c44407369522c3d28f0183da885d933a1b26196f
Timofei Davydik <timofei.davydik@helmes.com>
2025-05-08 20:10:56 +0300

    back camera

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function onScanFailure(error) {
 }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then((registration) => {
             console.log('Service Worker registered:', registration);
         })
